fa5fbbaffe0447507cce609bbeae77bf137a371d
YYB <2084395601@qq.com>
2023-11-20 15:06:24 +0800

    通用页面

---
+++ src/pages/generic/my.vue
@@ -0,0 +1,23 @@
+<template>
+	<!-- 右上角快捷按钮【用户】 -->
+	<view>
+		功能？
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
