5afe2f21fbc26f37ff0fad976f17419d402af9af
YYB <2084395601@qq.com>
2023-11-26 18:59:37 +0800

    数据可视化

--- src/pages/user/data.vue
+++ src/pages/user/data.vue
@@ -13,21 +13,23 @@
 			<text>个人数据分析</text>
 			<view class="bg"></view>
 		</view>
+		
 		<view class="datas">
 			<view class="midtitle">
 				本周学习数据
 			</view>
 			<!-- 核心知识点 -->
-			<view class="text">
-				掌握核心知识点情况
+			<view class="text">掌握核心知识点情况</view>
+			<!-- force-use-old-canvas="true" -->
+			<view @click="echarts.onClick" :prop="radar2" id="radar2" :change:prop="echarts.updateEcharts">
 			</view>
-			<view id="radar2" force-use-old-canvas="true"></view>
 			<!-- 知识点分布 -->
 			<view class="text">本周知识点分布</view>
-			<view id="radar1"></view>
+			<view @click="echarts.onClick" :prop="radar1" id="radar1" :change:prop="echarts.updateEcharts"></view>
 			<!-- 视频类型分布 -->
 			<view class="text">喜爱视频类型分布</view>
-			<view id="pie"></view>
+			<view @click="echarts.onClick" :prop="pie" id="pie" :change:prop="echarts.updateEcharts">
+			</view>
 			<!-- 学习时长 -->
 			<view class="text">总共学习时长
 				<text>
@@ -35,340 +37,373 @@
 				</text>
 				分钟
 			</view>
-			<view id="bar"></view>
+			<view @click="echarts.onClick" :prop="bar" id="bar" :change:prop="echarts.updateEcharts">
+			</view>
 			<!-- 积分统计 -->
 			<view class="text">本周活跃度分析</view>
-			<view id="polylines"></view>
+			<view @click="echarts.onClick" :prop="polylines" id="polylines" :change:prop="echarts.updateEcharts">
+			</view>
 		</view>
 	</view>
 </template>
 
-<script setup>
+<script>
 	import myheader from "@/component/header.vue"
-	import * as echarts from "echarts";
-	import {
-		onMounted,
-		onUnmounted,
-		ref
-	} from "vue";
-	// 核心知识点和学习时间统计
-	const sum = ref([78, 389])
-	// 核心知识点发布 雷达图
-	const key = ref([345, 724, 665, 615, 455])
-	// 科普知识点分布 雷达图
-	const science = ref([345, 725, 665, 615, 455])
-	// 喜爱视频类型  饼图
-	const favor = ref([10, 10, 10, 10, 10, 10])
-	// 学习时间  柱状图
-	const study = ref([39, 52, 49, 47, 19, 41, 31])
-	// 本周活跃度分析  折线图
-	const score = ref([150, 180, 175, 170, 55, 156, 99]);
-	//日期数据
-	const data = ref(['9-18', '9-19', '9-20', '9-21', '9-22', '9-23', '9-24'])
+	export default {
+		components: {
+			myheader: myheader
+		},
+		data() {
+			return {
+				sum: [78, 389],
+				radar1: {},
+				radar2: {},
+				pie: {},
+				bar: {},
+				polylines: {}
+			}
+		},
+		onLoad() {
+			this.echart()
+		},
+		methods: {
+			echart() {
+				// 核心知识点和学习时间统计
+				let sum = [78, 389]
+				// 核心知识点发布 雷达图
+				let key = [345, 724, 665, 615, 455]
+				// 科普知识点分布 雷达图
+				let science = [345, 725, 665, 615, 455]
+				// 喜爱视频类型  饼图
+				let favor = [10, 10, 10, 10, 10, 10]
+				// 学习时间  柱状图
+				let study = [39, 52, 49, 47, 19, 41, 31]
+				// 本周活跃度分析  折线图
+				let score = [150, 180, 175, 170, 55, 156, 99]
+				//日期数据
+				let data = ['9-18', '9-19', '9-20', '9-21', '9-22', '9-23', '9-24']
 
-	/// 声明定义一下echart
-	let echart = echarts;
+				this.radar1 = { // 饼图：本周知识点分布
+					tooltip: {
+						trigger: 'item'
+					},
+					legend: {
+						top: '5%',
+						left: 'center'
+					},
+					series: [{
+						name: 'Access From',
+						type: 'pie',
+						radius: ['40%', '70%'],
+						avoidLabelOverlap: false,
+						label: {
+							show: false,
+							position: 'center'
+						},
+						emphasis: {
+							label: {
+								show: true,
+								fontSize: 40,
+								fontWeight: 'bold'
+							}
+						},
+						labelLine: {
+							show: false
+						},
+						data: [{
+								value: 90,
+								name: '发展'
+							},
+							{
+								value: 50,
+								name: '历史'
+							},
+							{
+								value: 55,
+								name: '风险'
+							},
+							{
+								value: 80,
+								name: '原理'
+							},
+							{
+								value: 75,
+								name: '应用'
+							}
+						]
+					}]
+				}
+				this.radar2 = { // 雷达图：核心知识点掌握情况
+					color: '#83b6a2',
+					title: {
+						text: ''
+					},
+					legend: {},
+					radar: [{
+						indicator: [{
+								name: '历史',
+								max: 1000
+							},
+							{
+								name: '发展',
+								max: 1000
+							},
+							{
+								name: '原理',
+								max: 1000
+							},
+							{
+								name: '应用',
+								max: 1000
+							},
+							{
+								name: '风险',
+								max: 1000
+							},
+						],
+						radius: 100,
+						// top: 90,
+						startAngle: 90,
+						splitNumber: 4,
+						// shape: 'circle',
+						axisName: {
+							// formatter: '【{value}】',
+							fontSize: 14,
+							color: '#15a0ac',
+						},
+						splitArea: {
+							areaStyle: {
+								color: '#90c9b430',
+								shadowColor: 'none',
+								// shadowBlur: 10
+							}
+						},
+						axisLine: {
+							lineStyle: {
+								color: '#90c9b400'
+							}
+						},
+						splitLine: {
+							lineStyle: {
+								color: '#90c9b400'
+							}
+						}
+					}],
+					series: [{
+						name: 'Budget vs spending',
+						type: 'radar',
+						data: [{
+							value: key,
+							color: '#fff'
+						}]
+					}]
+				}
+				this.pie = { // 饼图：喜爱视频分布
+					title: {
 
-	onMounted(() => {
-		initChart()
-		sum.value = [0, 0]
-		// 核心知识点统计
-		for (var i = 0; i < key.value.length; i++) {
-			sum.value[0] += ~~key.value[i]
-		}
-		// 学习时间统计
-		for (var i = 0; i < study.value.length; i++) {
-			sum.value[1] += ~~study.value[i]
-		}
-	})
-	onUnmounted(() => {
-		echart.dispose
-	})
-
-	// 基础配置一下Echarts
-	function initChart() {
-		let radar1 = echart.init(document.getElementById("radar1"));
-		let radar2 = echart.init(document.getElementById("radar2"));
-		let pie = echart.init(document.getElementById("pie"));
-		let bar = echart.init(document.getElementById("bar"));
-		let polylines = echart.init(document.getElementById("polylines"));
-		// 把配置和数据放这里
-		// 雷达图：
-		radar1.setOption({
-			tooltip: {
-				trigger: 'item'
-			},
-			legend: {
-				top: '5%',
-				left: 'center'
-			},
-			series: [{
-				name: 'Access From',
-				type: 'pie',
-				radius: ['40%', '70%'],
-				avoidLabelOverlap: false,
-				label: {
-					show: false,
-					position: 'center'
-				},
-				emphasis: {
-					label: {
-						show: true,
-						fontSize: 40,
-						fontWeight: 'bold'
-					}
-				},
-				labelLine: {
-					show: false
-				},
-				data: [{
-						value: 90,
-						name: '发展'
+						//  subtext: 'Fake Data',
+						left: 'center'
 					},
-					{
-						value: 50,
-						name: '历史'
+					tooltip: {
+						trigger: 'item'
 					},
-					{
-						value: 55,
-						name: '风险'
+					legend: {
+						orient: 'vertical',
+						left: 'left'
 					},
-					{
-						value: 80,
-						name: '原理'
-					},
-					{
-						value: 75,
-						name: '应用'
-					}
-				]
-			}]
-		});
-		// 雷达图：核心知识点掌握情况
-		radar2.setOption({
-			color: '#83b6a2',
-			title: {
-				text: ''
-			},
-			legend: {},
-			radar: [{
-				indicator: [{
-						name: '历史',
-						max: 1000
-					},
-					{
-						name: '发展',
-						max: 1000
-					},
-					{
-						name: '原理',
-						max: 1000
-					},
-					{
-						name: '应用',
-						max: 1000
-					},
-					{
-						name: '风险',
-						max: 1000
-					},
-				],
-				radius: 100,
-				// top: 90,
-				startAngle: 90,
-				splitNumber: 4,
-				// shape: 'circle',
-				axisName: {
-					// formatter: '【{value}】',
-					fontSize: 14,
-					color: '#15a0ac',
-				},
-				splitArea: {
-					areaStyle: {
-						color: '#90c9b430',
-						shadowColor: 'none',
-						// shadowBlur: 10
-					}
-				},
-				axisLine: {
-					lineStyle: {
-						color: '#90c9b400'
-					}
-				},
-				splitLine: {
-					lineStyle: {
-						color: '#90c9b400'
-					}
+					series: [{
+						name: 'Access From',
+						type: 'pie',
+						radius: '50%',
+						data: [{
+								value: 999,
+								name: '发展'
+							},
+							{
+								value: 735,
+								name: '应用'
+							},
+							{
+								value: 580,
+								name: '原理'
+							},
+							{
+								value: 400,
+								name: '历史'
+							},
+							{
+								value: 400,
+								name: '风险'
+							}
+						],
+						emphasis: {
+							itemStyle: {
+								shadowBlur: 10,
+								shadowOffsetX: 0,
+								shadowColor: 'rgba(0, 0, 0, 0.5)'
+							}
+						}
+					}]
 				}
-			}],
-			series: [{
-				name: 'Budget vs spending',
-				type: 'radar',
-				data: [{
-					value: key.value,
-					color: '#fff'
-				}]
-			}]
-		})
-		// 饼图：喜爱视频分布
-		pie.setOption({
-			title: {
-				
-				//  subtext: 'Fake Data',
-				left: 'center'
-			},
-			tooltip: {
-				trigger: 'item'
-			},
-			legend: {
-				orient: 'vertical',
-				left: 'left'
-			},
-			series: [{
-				name: 'Access From',
-				type: 'pie',
-				radius: '50%',
-				data: [{
-						value: 999,
-						name: '发展'
+				this.bar = { // 柱状图：学习时长
+					color: ['#90c9b4'],
+					tooltip: {
+						trigger: 'axis',
+						axisPointer: {
+							type: 'shadow'
+						}
 					},
-					{
-						value: 735,
-						name: '应用'
+					grid: {
+						top: '10%',
+						left: '3%',
+						right: '4%',
+						bottom: '10%',
+						containLabel: true
 					},
-					{
-						value: 580,
-						name: '原理'
+					xAxis: [{
+						type: 'category',
+						data: data,
+						axisTick: {
+							alignWithLabel: true
+						}
+					}],
+					yAxis: [{
+						type: 'value'
+					}],
+					series: [{
+						name: 'Direct',
+						type: 'bar',
+						barWidth: '60%',
+						data: study,
+						label: {
+							show: true, //开启显示
+							position: 'top', //在上方显示
+							// formatter: '{c}',//显示百分号
+							textStyle: { //数值样式
+								color: '#80b29f', //字体颜色
+								fontSize: 14 //字体大小
+							}
+						}
+					}]
+				}
+				this.polylines = { // 折线图：本周活跃度分析
+					visualMap: [{
+						show: false,
+						type: 'continuous',
+						seriesIndex: 0,
+						min: 0,
+						max: 400
+					}],
+					title: {
+						// left: 'center',
+						// text: 'Gradient along the y axis'
 					},
-					{
-						value: 400,
-						name: '历史'
+					tooltip: {
+						trigger: 'axis'
 					},
-					{
-						value: 400,
-						name: '风险'
-					}
-				],
-				emphasis: {
-					itemStyle: {
-						shadowBlur: 10,
-						shadowOffsetX: 0,
-						shadowColor: 'rgba(0, 0, 0, 0.5)'
-					}
-				}
-			}]
-		})
-		// 柱状图：学习时长
-		bar.setOption({
-			color: ['#90c9b4'],
-			tooltip: {
-				trigger: 'axis',
-				axisPointer: {
-					type: 'shadow'
-				}
-			},
-			grid: {
-				top: '10%',
-				left: '3%',
-				right: '4%',
-				bottom: '10%',
-				containLabel: true
-			},
-			xAxis: [{
-				type: 'category',
-				data: data.value,
-				axisTick: {
-					alignWithLabel: true
-				}
-			}],
-			yAxis: [{
-				type: 'value'
-			}],
-			series: [{
-				name: 'Direct',
-				type: 'bar',
-				barWidth: '60%',
-				data: study.value,
-				label: {
-					show: true, //开启显示
-					position: 'top', //在上方显示
-					// formatter: '{c}',//显示百分号
-					textStyle: { //数值样式
-						color: '#80b29f', //字体颜色
-						fontSize: 14 //字体大小
-					}
-				}
-			}]
-		})
-		// 折线图：本周活跃度分析
-		polylines.setOption({
-			visualMap: [{
-				show: false,
-				type: 'continuous',
-				seriesIndex: 0,
-				min: 0,
-				max: 400
-			}],
-			title: {
-				// left: 'center',
-				// text: 'Gradient along the y axis'
-			},
-			tooltip: {
-				trigger: 'axis'
-			},
-			xAxis: {
-				type: 'category',
-				boundaryGap: false,
-				data: data.value
-			},
-			yAxis: {},
-			grid: {
-				top: '14%',
-				bottom: '16%'
-			},
-			series: {
-				type: 'line',
-				showSymbol: false,
-				data: score.value,
-				lineStyle: {
-					normal: {
-						width: 4,
-						color: {
-							type: 'linear', // radial   linear
-							x: 0,
-							y: 0,
-							x2: 0,
-							y2: 1,
-							colorStops: [{
-									offset: 0,
-									color: '#118a90' // 0% 处的颜色 
-								},
-								{
-									offset: 1,
-									color: '#a1e0c8' // 100% 处的颜色 
+					xAxis: {
+						type: 'category',
+						boundaryGap: false,
+						data: data
+					},
+					yAxis: {},
+					grid: {
+						top: '14%',
+						bottom: '16%'
+					},
+					series: {
+						type: 'line',
+						showSymbol: false,
+						data: score,
+						lineStyle: {
+							normal: {
+								width: 4,
+								color: {
+									type: 'linear', // radial   linear
+									x: 0,
+									y: 0,
+									x2: 0,
+									y2: 1,
+									colorStops: [{
+											offset: 0,
+											color: '#118a90' // 0% 处的颜色 
+										},
+										{
+											offset: 1,
+											color: '#a1e0c8' // 100% 处的颜色 
+										}
+									],
+									global: false // 缺省为 false 
 								}
-							],
-							global: false // 缺省为 false 
+							}
+
 						}
 					}
-
 				}
+			},
+
+			onViewClick(options) {
+				console.log(options)
 			}
-		})
+		}
 
-
-
-
-		window.onresize = function() {
-			//自适应大小
-			chart.resize();
-			chart2.resize();
-			pie.resize();
-			bar.resize();
-			polylines.resize();
-		};
 	}
 </script>
+<script module="echarts" lang="renderjs">
+	let radar1
+	let radar2
+	let pie
+	let bar
+	let polylines
+	export default {
+		mounted() {
+			if (typeof window.echarts === 'function') {
+				this.initEcharts()
+			} else {
+				// 动态引入较大类库避免影响页面展示
+				const script = document.createElement('script')
+				// view 层的页面运行在 www 根目录，其相对路径相对于 www 计算
+				script.src = 'static/js/echarts.js'
+				script.onload = this.initEcharts.bind(this)
+				document.head.appendChild(script)
+			}
+		},
+		methods: {
+			initEcharts() {
+				radar1 = echarts.init(document.getElementById('radar1'))
+				radar2 = echarts.init(document.getElementById('radar2'))
+				pie = echarts.init(document.getElementById('pie'))
+				bar = echarts.init(document.getElementById('bar'))
+				polylines = echarts.init(document.getElementById('polylines'))
+				// 观测更新的数据在 view 层可以直接访问到
+				radar1.setOption(this.radar1)
+				radar2.setOption(this.radar2)
+				pie.setOption(this.pie)
+				bar.setOption(this.bar)
+				polylines.setOption(this.polylines)
+				// 	//自适应大小
+				// radar1.resize();
+				// radar2.resize();
+				// pie.resize();
+				// bar.resize();
+				// polylines.resize();
+			},
+			updateEcharts(newValue, oldValue, ownerInstance, instance) {
+				// 监听 service 层数据变更
+				// if (myChart != undefined) {
+				// 	myChart.setOption(newValue)
+				// }
+			},
+			onClick(event, ownerInstance) {
+				// 调用 service 层的方法
+				ownerInstance.callMethod('onViewClick', {
+					test: 'test'
+				})
+			}
+		}
+	}
+</script>
+
 
 <style lang="scss">
 	.data {
@@ -445,7 +480,7 @@
 		#pie,
 		#bar,
 		#polylines {
-			width: 680rpx;
+			width: 640rpx;
 			height: 530rpx;
 			background-color: #fff;
 			margin: 20rpx auto;
